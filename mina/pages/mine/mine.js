dcef738967588d0a0f5c1020e9741070a232288f
BeijiYang <langyuyang@outlook.com>
2017-07-08 12:20:08 +0800

    relogin after logout

--- mina/pages/mine/mine.js
+++ mina/pages/mine/mine.js
@@ -26,7 +26,69 @@ Page({
       console.log(e);
     }
   },
-  
+
+  login: function () {
+    let that = this
+    console.log('login~~~');
+    //检测登录态
+    getApp().checkToken().then(
+      function (res) {
+        console.log(res);
+        if(parseInt(res.statusCode) === 200){
+          console.log(res)
+          console.log('未过期');
+            //token 未过期，并且在本生命周期一直有效
+      } else {
+           console.log(res.data.error);
+            //登录态过期
+            console.log('过期了！');
+            that.reGetUserInfo()
+        }
+      }
+    ).catch(
+      function (err) {
+        console.log(err);
+        wx.showToast({
+          title: '服务器未响应',
+          icon: 'loading',
+          duration: 2000
+        })
+      }
+    )
+  },
+
+  //重新请求使用用户信息的授权
+  reGetUserInfo: function () {
+    console.log('reGetUserInfo~~~');
+    let that = this
+    wx.openSetting({
+      success:(res)=>{
+        let balbal = that
+        console.log(that.data.test);
+        console.log("授权结果..")
+        console.log(res)
+          res.authSetting = {
+           "scope.userInfo": true,
+         }
+         getApp().logIn()
+         .then(
+           (res) => {
+             console.log("NEW PROMISE!!!")
+             that.setData({
+               test: true,
+               userInfo: wx.getStorageSync('user'),
+             })
+           }
+         )
+         .catch(
+           (err) => {
+             console.log(err);
+           }
+         )
+      }
+    })
+  },
+
   //退出登录
   quit: function() {
     try {
