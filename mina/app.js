c34a6fd4ab367a86d65ef7ef1ac9ac88f983ea93
BeijiYang <langyuyang@outlook.com>
2017-07-08 11:17:02 +0800

    get token from server

--- mina/app.js
+++ mina/app.js
@@ -1,29 +1,46 @@
-//app.js
+const config = require('./config/config.js');
+
 App({
   onLaunch: function() {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-
-  getUserInfo: function(cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function(res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
+    this.logIn()
+ },
+  logIn: function() {
+    return new Promise(function(resolve,reject){
+      wx.login({
+        success: (res) => {
+          let code = res.code
+          console.log(code);
+          if (code) {
+            wx.getUserInfo({
+              success: (res) => {
+                wx.setStorageSync('user', res.userInfo)
+                wx.request({
+                  url: `${config.api}/login`,
+                  method: 'POST',
+                  data: {
+                    code: code
+                  },
+                  header: {
+                    'content-type': 'application/json'
+                  },
+                  success: function (res) {
+                    console.log(res.data.token)
+                    wx.setStorageSync('token', res.data.token)
+                    resolve(res)
+                  }
+                })
+              },
+              fail: function () {
+                console.log("如果用户点了拒绝，则短期内调用不会出现弹窗，而是直接进入 fail 回调");
+              }
+            })
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+            reject(err)
+          }
         }
-      })
-    }
-  },
+      });
 
-  globalData: {
-    userInfo: null
-  }
+      })
+    },
 })
