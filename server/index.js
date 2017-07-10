0695756fc88f33b32c87f3923c94b4c5e31a5025
BeijiYang <langyuyang@outlook.com>
2017-07-10 17:41:10 +0800

    fix a warning

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express =  require('express')
 const app = express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mina');
+mongoose.connect('mongodb://localhost:27017/mina', {
+  useMongoClient: true,
+});
 
 const db = mongoose.connection;
 db.on('error', console.log);
